6deb00ae51479b7a11381e72f167090d36fbe12d
Jake Mola <mola.jake@student.ccm.edu>
2013-09-05 20:11:33 -0400

    dfgn

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Hello world from String";
+            char myChar = 'a';
+            int myInt = 5;
+            float myFloat = 5f;
+            double myDbl = 5;
+            bool myBool = false;
+            int mynewInt = 0;                     
+            
+            Console.WriteLine(MyFunc(5, 3.1));
+        }
+
+        public static float MyFunc(float a; float b;)
+        {
+            return a + b;
+        }
+
+    }
+}
